15e7fc8b4cd3dedba4f3ed1082e198e5588f634f
Ahsan Habib <ahsanpub16b@gmail.com>
2022-03-25 23:56:04 +0600

    Local Storage worked here!

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -13,11 +14,40 @@ const Shop = () => {
         .then(data => setProducts(data))
     },[]);
 
+    //cart load -> code er bahir er jinis load
+    useEffect(() => {
+        const storedCart = getStoredCart();
+        const savedCart = [];
+
+        for (const id in storedCart) {
+            const addedProduct = products.find(product => product.id === id);
+            if(addedProduct){
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct);
+            }
+        }
+        setCart(savedCart);
+
+    },[products]);
+
     // props to add to cart button handler 
-    const handleAddToCart = (product) =>{
-        console.log(product);
-        const newCart = [...cart , product];
+    const handleAddToCart = (selectedProduct) =>{
+        console.log(selectedProduct);
+        let newCart = [];
+        const exists = cart.find(product => product.id === selectedProduct.id);
+        if(!exists){
+            selectedProduct.quantity = 1;
+            newCart = [...cart , selectedProduct];
+        }
+        else{
+            const rest = cart.filter(product => product.id !== selectedProduct.id);
+            exists.quantity = exists.quantity + 1 ; 
+            newCart = [...rest , exists];
+        }
+       
         setCart(newCart);
+        addToDb(selectedProduct.id)
     }
 
     return (
